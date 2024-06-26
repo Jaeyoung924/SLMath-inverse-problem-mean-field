0f84eff25524c3c559d4012a6cffe9bb1ba5ea79
jwmhunter <hunter.926@osu.edu>
2024-06-26 14:51:13 -0700

    Added loop to run 30 tests and average the results

--- main2.m
+++ main2.m
@@ -11,6 +11,7 @@ stop_criterion = 10^(-8);
 max_iter = 10000;
 ht = 0.2; Nt = int64(1/ht); % for U
 hs = 10;
+n_tests = 30;
 
 disp(['     Total N','     Total M','           N','           M','         h_s'])
 disp([Total_N, Total_M, N, M, hs])
@@ -44,6 +45,19 @@ toc
 
 Total_U_ref = Total_U_ref(end,:);
 
+% --- Uncomment the lines below to run multiple tests ---
+Expected_Theta = 0;
+Expected_L = 0;
+Expected_mean = 0;
+Expected_std = 0;
+Expected_traj = 0;
+% --- Uncomment the lines above to run multiple tests ---
+
+% --- Uncomment the lines below to run multiple tests ---
+for tests = 1:n_tests
+rng(n_tests);
+% --- Uncomment the lines aboves to run multiple tests ---
+
 Theta = rand(N,1); % Initial data for Theta
 % Theta = -1 + randn(N,1); % Initial data for Theta
 idx_per = randperm(Total_M); idx_used = idx_per(1:M);
@@ -51,7 +65,7 @@ X = Total_X(idx_used); U_ref = Total_U_ref(idx_used);
 
 %% %% Forward Euler method
 s = 0;
-L = []; mean_theta = []; var_theta = [];
+L = []; mean_theta = []; std_theta = [];
 s_traj = s;
 Theta_traj = Theta;
 tic
@@ -64,7 +78,7 @@ while (s<max_iter)
 %     end
     Theta = Theta-grd; s = s+1;
     mean_theta = [mean_theta, sum(Theta)/N];
-    var_theta = [var_theta, var(Theta)];
+    std_theta = [std_theta, std(Theta)];
     L = [L, sum((U(end,:)-U_ref).^2)/M];
 %     if size(L,2)~=1 && L(end-1)-L(end)<0
 %         hs = hs*0.9;
@@ -80,6 +94,24 @@ end
 U = Euler2_U(X,Theta,N,M,ht,Nt,f_id); % size: (Nt+1)*M
 L = [L, sum((U(end,:)-U_ref).^2)/M];
 toc
+
+% --- Uncomment the lines below to run multiple tests ---
+Expected_Theta = Expected_Theta + Theta;
+Expected_L = Expected_L + L;
+Expected_mean = Expected_mean + mean_theta;
+Expected_std = Expected_std + std_theta;
+Expected_traj = Expected_traj + Theta_traj;
+end %for loop over tests
+% --- Uncomment the lines above to run multiple tests ---
+
+% --- Uncomment the lines below to run multiple tests ---
+Theta = Expected_Theta / n_tests;
+U = Euler2_U(X,Theta,N,M,ht,Nt,f_id); % size: (Nt+1)*M
+L = Expected_L / n_tests;
+mean_theta = Expected_mean / n_tests;
+std_theta = Expected_std / n_tests;
+Theta_traj = Expected_traj / n_tests;
+% --- Uncomment the lines above to run multiple tests ---
 
 %% Plotting
 % figure; subplot(1,2,1); hold on
@@ -98,7 +130,7 @@ title("test, N="+N)
 
 figure; subplot(1,2,1); semilogy(0:s,L); title('L');
 subplot(1,2,2); hold on
-plot(1:s,mean_theta,'r'); plot(1:s,var_theta,'b');
+plot(1:s,mean_theta,'r'); plot(1:s,std_theta,'b');
 legend('mean of theta','variance of theta');
 
 %% Plot2 
